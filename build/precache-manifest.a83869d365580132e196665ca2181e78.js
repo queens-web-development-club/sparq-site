d46f14fc9aa4f78f5385acfe475fe1ea26ed6db4
CPayne6 <cpaynetrayne6@gmail.com>
2020-05-12 13:14:07 -0400

    Initial Site

---
+++ build/precache-manifest.a83869d365580132e196665ca2181e78.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5d56cd480ab899ae5037719679672f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ca8e72d52743683c3e1",
+    "url": "/static/css/main.edc16925.chunk.css"
+  },
+  {
+    "revision": "bc1a2dbe07f55ba2c98d",
+    "url": "/static/js/2.76a7397e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.76a7397e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ca8e72d52743683c3e1",
+    "url": "/static/js/main.a86c6bc9.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "ae47095a2d8d83340a4217af971eab9e",
+    "url": "/static/media/CopyrightSparQ.ae47095a.svg"
+  },
+  {
+    "revision": "266ebaeb2d38ba25388af5008815ac5f",
+    "url": "/static/media/Dunin.266ebaeb.jpg"
+  },
+  {
+    "revision": "fcf0ea7b172cee6ec08cda5d75a358cc",
+    "url": "/static/media/Facebook.fcf0ea7b.svg"
+  },
+  {
+    "revision": "4366d3195ff31b3d0e171ac4c54befbc",
+    "url": "/static/media/IMG_8854.4366d319.jpg"
+  },
+  {
+    "revision": "0cb05779e5a34de7acacfde23fda8f77",
+    "url": "/static/media/IMG_8867.0cb05779.jpg"
+  },
+  {
+    "revision": "e2000076c2d44558af72d9b14b9523ae",
+    "url": "/static/media/Instagram.e2000076.svg"
+  },
+  {
+    "revision": "a761714d2c6db23125398648875c76db",
+    "url": "/static/media/Send.a761714d.svg"
+  },
+  {
+    "revision": "f9e429c9a3e783da11f5907191c617d4",
+    "url": "/static/media/SparQ.f9e429c9.svg"
+  },
+  {
+    "revision": "bb9ef7c82296fe2595c735dfa221cd9a",
+    "url": "/static/media/WindowPicture.bb9ef7c8.jpg"
+  },
+  {
+    "revision": "f5ec36c1b009111528007fb340a428db",
+    "url": "/static/media/workshop.f5ec36c1.jpg"
+  }
+]);
